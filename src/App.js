9ee49e72f9ca104bc934900ba070290af53c0ba5
alexabadp <aalex.abad96@gmail.com>
2023-04-05 13:13:35 -0500

    mostrar y ocultar Formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Formulario from "./components/Formulario/Formulario";
+import MiOrg from "./components/MiOrg";
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true);
+
+  //Ternario --> condicion ? seMuestra: noSeMuestra
+  //condicion && seMuestra
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  };
+
   return (
     <div>
       <Header />
-      <Formulario />
+      {mostrarFormulario && <Formulario />}
+
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
